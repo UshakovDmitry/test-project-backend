7ae25a06716d5696b6641f8b08401c53fd5980c5
Dmitry Ushakov <dimau95@yandex.ru>
2023-07-25 09:48:18 +0600

    added validation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,17 +1,30 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
 
 exports.register = async (req, res, next) => {
   const { email, password, name } = req.body;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+      message: "Некорректные данные при регистрации",
+    });
+  }
 
   try {
     let user = await User.findOne({ email });
 
     if (user) {
-      return res.status(400).json({ message: 'Пользователь уже существует' });
+      return res.status(400).json({ message: "Пользователь уже существует" });
     }
 
-    user = new User({ email, password, name });
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    user = new User({ email, password: hashedPassword, name });
     await user.save();
 
     res.status(201).json(user);
@@ -23,18 +36,35 @@ exports.register = async (req, res, next) => {
 exports.login = async (req, res, next) => {
   const { email, password } = req.body;
 
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+      message: "Некорректные данные при входе в систему",
+    });
+  }
+
   try {
     const user = await User.findOne({ email });
 
-    if (!user || user.password !== password) {
-      return res.status(401).json({ message: 'Неверный email или пароль' });
+    if (!user) {
+      return res.status(401).json({ message: "Пользователь не найден" });
     }
 
-    const token = jwt.sign({ userId: user.id }, 'secret', { expiresIn: '1h' });
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ message: "Неверный пароль, попробуйте снова" });
+    }
+
+    const token = jwt.sign({ userId: user.id }, "secret", { expiresIn: "1h" });
 
     res.status(200).json({
       token,
-      user: { email: user.email, name: user.name }
+      user: { email: user.email, name: user.name },
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const { check } = require('express-validator');
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+router.post('/register', [
+  check('email', 'Некорректный email').isEmail(),
+  check('password', 'Минимальная длина пароля 6 символов').isLength({ min: 6 })
+], authController.register);
+
+router.post('/login', [
+  check('email', 'Введите корректный email').normalizeEmail().isEmail(),
+  check('password', 'Введите пароль').exists()
+], authController.login);
 
 module.exports = router;
