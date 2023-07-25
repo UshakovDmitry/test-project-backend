d7e9d7ac914420c443aff858f75d3046fdfe273f
Dmitry Ushakov <dimau95@yandex.ru>
2023-07-25 15:22:09 +0600

    ref errors message

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,7 +6,7 @@ exports.register = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({ message: "Пустые поля не принимаю" });
   }
 
   const { email, password, name } = req.body;
